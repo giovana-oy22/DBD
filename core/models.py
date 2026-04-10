ef7547b153450408acfdd32d8f29b44e0df481b9
Giovana Oyakawa <giovana.oyakawa@usp.br>
2026-04-10 19:00:26 -0300

    🎉 New Django Project

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.FloatField()
+
+    def __str__(self):
+        return self.nome
