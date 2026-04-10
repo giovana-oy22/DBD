d7627285791f12a02620da3c6203bf7885b877b5
Giovana Oyakawa <giovana.oyakawa@usp.br>
2026-04-10 20:33:28 -0300

    ✨ New Entities

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Cliente, Pedido, ItemPedido
 
 admin.site.register(Produto)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
+admin.site.register(ItemPedido)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,37 @@
 from django.db import models
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.FloatField()
+# CLIENTES
+class Cliente(models.Model):
+    nome = models.TextField()
+    endereco = models.TextField()
 
     def __str__(self):
         return self.nome
+
+
+# PRODUTOS
+class Produto(models.Model):
+    nome = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+# PEDIDOS
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido {self.id}"
+
+
+# ITENS DO PEDIDO
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.produto.nome} ({self.quantidade})"
